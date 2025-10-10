ee9d953bf3ef5b05baf0d22652f9532705d30b61
Yuya, Matsumoto <yuya2005m@gmail.com>
2025-10-11 08:54:47 +0900

    fix: 微調整

--- src/hooks/useFirstRole.ts
+++ src/hooks/useFirstRole.ts
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { FirstState } from "@/types/connect4";
 
-export default function useFirstRole(roomId: string) {
+export default function useFirstRole() {
 	const [firstRole, setFirstRole] = useState<FirstState>("random");
 
 	return {
