4eea15e2a752dc0cdbaa158d87b9646f4d420559
Yuya, Matsumoto <yuya2005m@gmail.com>
2025-10-17 14:55:45 +0900

    fix: onRestart関数からsetCurrentRoleを削除

--- src/libs/connect4/onRestart.ts
+++ src/libs/connect4/onRestart.ts
@@ -1,11 +1,9 @@
 import { OnRestartProps } from "@/types/connect4";
-import { Role } from "@/constants/connect4";
 import { createEmptyBoard } from "@/libs/connect4/createEmptyBoard";
 
-export const onRestart = ({ setIsWin, setBoard, setCurrentRole, setCanPlay, setLastPosition }: OnRestartProps) => {
+export const onRestart = ({ setIsWin, setBoard, setCanPlay, setLastPosition }: OnRestartProps) => {
 	setIsWin(false);
 	setBoard(createEmptyBoard());
-	setCurrentRole(Role.RED);
 	setCanPlay(true);
 	setLastPosition({ row: null, col: null });
 }
