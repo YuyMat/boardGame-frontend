5d94ed75ccde35968283a3e9d1e95de23756a692
Yuya, Matsumoto <yuya2005m@gmail.com>
2025-11-19 07:57:47 +0900

    path error fix

--- src/components/Connect4/index.ts
+++ src/components/Connect4/index.ts
@@ -1,3 +1,3 @@
 export { default as Board } from "./Board";
-export { default as ShowTurn } from "./PlayerInfo";
+export { default as ShowTurn } from "./showTurn";
 export { default as ReShowResult } from "./ReShowResult";
--- src/components/Reversi/index.ts
+++ src/components/Reversi/index.ts
@@ -1,5 +1,5 @@
 export { default as Board } from "./Board";
 export { default as Result } from "./Result";
 export { default as SkipTurn } from "./SkipTurn";
-export { default as ShowTurn } from "./PlayerInfo";
+export { default as ShowTurn } from "./showTurn";
 export { default as TurnInfo } from "./TurnInfo";
