c273ce4753c49315477dcd9a156eabaae2559cad
Yuya, Matsumoto <yuya2005m@gmail.com>
2026-01-04 13:44:20 +0900

    fix: Youからあなたに変更

--- src/components/Reversi/ReversiScoreBoard.tsx
+++ src/components/Reversi/ReversiScoreBoard.tsx
@@ -25,7 +25,7 @@ export default function ReversiScoreBoard({ blackCount, whiteCount, currentRole,
 					<div className="w-7 h-7 rounded-full bg-linear-to-br from-slate-700 via-slate-900 to-black shadow-[0_8px_32px_rgba(0,0,0,0.8),inset_0_2px_8px_rgba(255,255,255,0.1)] border border-white/10">
 					</div>
 					<span className="text-2xl font-bold text-gray-800 leading-none mt-1">{blackCount}</span>
-					{playerRole === Role.BLACK && <div className="ml-5 text-gray-500">You</div>}
+					{playerRole === Role.BLACK && <div className="ml-5 text-gray-500">あなた</div>}
 				</div>
 				{currentRole === Role.BLACK && (
 					<div className="w-2.5 h-2.5 rounded-full bg-blue-400 animate-pulse mr-1" />
@@ -44,7 +44,7 @@ export default function ReversiScoreBoard({ blackCount, whiteCount, currentRole,
 					<div className="w-2.5 h-2.5 md:w-3 md:h-3 rounded-full bg-blue-400 animate-pulse ml-1" />
 				)}
 				<div className="flex items-center gap-3">
-					{playerRole === Role.WHITE && <div className="mr-5 text-gray-500">You</div>}
+					{playerRole === Role.WHITE && <div className="mr-5 text-gray-500">あなた</div>}
 					<span className="text-2xl font-bold text-gray-800 leading-none mt-1">{whiteCount}</span>
 					<div className="w-7 h-7 rounded-full bg-linear-to-br from-white via-gray-100 to-gray-200 shadow-[0_8px_32px_rgba(255,255,255,0.4),inset_0_-2px_8px_rgba(0,0,0,0.1)] border border-white/30">
 					</div>
